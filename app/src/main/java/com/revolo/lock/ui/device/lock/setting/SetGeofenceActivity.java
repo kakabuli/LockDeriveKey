b93aeb2347b4714510a4513305f51c68720564db
wengmaowei <wengmaowei@kaadas.com>
2021-03-02 15:29:42 +0800

    [Added] 添加一些地理围栏需要用的的库

---
+++ app/src/main/java/com/revolo/lock/ui/device/lock/setting/SetGeofenceActivity.java
@@ -0,0 +1,43 @@
+package com.revolo.lock.ui.device.lock.setting;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.revolo.lock.R;
+import com.revolo.lock.base.BaseActivity;
+
+/**
+ * author :
+ * time   : 2021/3/2
+ * E-mail : wengmaowei@kaadas.com
+ * desc   :
+ */
+public class SetGeofenceActivity extends BaseActivity {
+    @Override
+    public void initData(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_set_geofence;
+    }
+
+    @Override
+    public void initView(@Nullable Bundle savedInstanceState, @Nullable View contentView) {
+
+    }
+
+    @Override
+    public void doBusiness() {
+
+    }
+
+    @Override
+    public void onDebouncingClick(@NonNull View view) {
+
+    }
+}
