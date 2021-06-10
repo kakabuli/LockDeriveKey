dac3cceb2ded7c6068073c7129e55208a4838cf3
yisiao@kaadas.com <yisiao@kaadas.com>
2021-06-10 17:14:49 +0800

    20210609 补充加载dialog超时处理

--- app/src/main/java/com/revolo/lock/dialog/iosloading/CustomerLoadingDialog.java
+++ app/src/main/java/com/revolo/lock/dialog/iosloading/CustomerLoadingDialog.java
@@ -2,11 +2,16 @@ package com.revolo.lock.dialog.iosloading;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
 import com.revolo.lock.R;
+
+import java.util.logging.Handler;
 
 /**
  * author :
@@ -15,6 +20,7 @@ import com.revolo.lock.R;
  * desc   :
  */
 public class CustomerLoadingDialog extends Dialog {
+    private static int MSG_DIALOG_SHOW_OUT_TIME = 254;
 
     public CustomerLoadingDialog(Context context) {
         super(context);
@@ -24,13 +30,13 @@ public class CustomerLoadingDialog extends Dialog {
         super(context, themeResId);
     }
 
-    public static class Builder{
+    public static class Builder {
 
         private Context context;
         private String message;
-        private boolean isShowMessage=true;
-        private boolean isCancelable=false;
-        private boolean isCancelOutside=false;
+        private boolean isShowMessage = true;
+        private boolean isCancelable = false;
+        private boolean isCancelOutside = false;
 
 
         public Builder(Context context) {
@@ -39,65 +45,91 @@ public class CustomerLoadingDialog extends Dialog {
 
         /**
          * 设置提示信息
+         *
          * @param message
          * @return
          */
 
-        public Builder setMessage(String message){
-            this.message=message;
+        public Builder setMessage(String message) {
+            this.message = message;
             return this;
         }
 
         /**
          * 设置是否显示提示信息
+         *
          * @param isShowMessage
          * @return
          */
-        public Builder setShowMessage(boolean isShowMessage){
-            this.isShowMessage=isShowMessage;
+        public Builder setShowMessage(boolean isShowMessage) {
+            this.isShowMessage = isShowMessage;
             return this;
         }
 
         /**
          * 设置是否可以按返回键取消
+         *
          * @param isCancelable
          * @return
          */
 
-        public Builder setCancelable(boolean isCancelable){
-            this.isCancelable=isCancelable;
+        public Builder setCancelable(boolean isCancelable) {
+            this.isCancelable = isCancelable;
             return this;
         }
 
         /**
          * 设置是否可以取消
+         *
          * @param isCancelOutside
          * @return
          */
-        public Builder setCancelOutside(boolean isCancelOutside){
-            this.isCancelOutside=isCancelOutside;
+        public Builder setCancelOutside(boolean isCancelOutside) {
+            this.isCancelOutside = isCancelOutside;
             return this;
         }
 
-        public CustomerLoadingDialog create(){
+        public CustomerLoadingDialog create() {
 
             LayoutInflater inflater = LayoutInflater.from(context);
-            View view=inflater.inflate(R.layout.dialog_loading,null);
-            CustomerLoadingDialog loadingDialog=new CustomerLoadingDialog(context,R.style.IosDialogStyle);
-            TextView msgText= view.findViewById(R.id.tipTextView);
-            if(isShowMessage){
+            View view = inflater.inflate(R.layout.dialog_loading, null);
+            CustomerLoadingDialog loadingDialog = new CustomerLoadingDialog(context, R.style.IosDialogStyle);
+            TextView msgText = view.findViewById(R.id.tipTextView);
+            if (isShowMessage) {
                 msgText.setText(message);
-            }else{
+            } else {
                 msgText.setVisibility(View.GONE);
             }
             loadingDialog.setContentView(view);
             loadingDialog.setCancelable(isCancelable);
             loadingDialog.setCanceledOnTouchOutside(isCancelOutside);
-            return  loadingDialog;
+            return loadingDialog;
 
         }
 
 
     }
 
+    @Override
+    public void show() {
+        super.show();
+        mHandler.sendEmptyMessageDelayed(MSG_DIALOG_SHOW_OUT_TIME, 5000);
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        mHandler.removeMessages(MSG_DIALOG_SHOW_OUT_TIME);
+    }
+
+    private android.os.Handler mHandler = new android.os.Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (msg.what == MSG_DIALOG_SHOW_OUT_TIME) {
+                if (CustomerLoadingDialog.this.isShowing()) {
+                    dismiss();
+                }
+            }
+        }
+    };
 }
